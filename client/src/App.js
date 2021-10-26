f984bb8e499f5e7265e86eaea07f0885c24c098a
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2021-10-26 13:51:59 -0700

    commiting

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [rR, setRR] = useState('');
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>hello</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
